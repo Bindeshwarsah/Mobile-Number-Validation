2e486b886769a60ee870ca824c175c5f8c68b6cc
Bindeshwarsah <83946613+Bindeshwarsah@users.noreply.github.com>
2023-09-27 17:47:06 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ SubmitBtn.addEventListener('click', function () {
             if(PhoneNumber.charAt(0)==6|| PhoneNumber.charAt(0)==7|| PhoneNumber.charAt(0)==8|| PhoneNumber.charAt(0)==9){
                 Result.innerHTML="your mobile number is successfully verified"
                 Result.style.color="green";
-                PhoneNumber.style.outlineColor="green";
+               
             }
             else{
                 Result.innerHTML="Enter valid mobile number"
+                Result.style.color="red";
             }
         }
     else {
         Result.innerHTML = "Enter 10 digit number";
+        Result.style.color="red";
     }
 });
